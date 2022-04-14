f19010df141367a01d3e747aa4a189ae94977b5d
britgand <jonathan.breitgand@gmail.com>
2022-04-14 19:45:22 +0300

    fixed cam

--- ball_cam_trans.gd
+++ ball_cam_trans.gd
@@ -9,24 +9,44 @@ var def_trans = global_transform.basis
 var def_trans_pos = Vector3.ZERO
 var def_self
 var free_cam = false
+var fake_cam #= $fake_cam
+var real_cam #= $Camera
+var t_lerp = 0
+var lerping = false
+var quat_of_self_def = Quat()
+var quat_of_self = Quat()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("ready")
 	def_camera = $Camera.transform
 	def_trans_pos = $Camera.transform.origin
 	def_self = transform
-
+	fake_cam = $fake_cam
+	
+	real_cam = $Camera
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if is_ball_cam:
 		look_at(get_parent().get_parent().get_parent().get_parent().get_node("ball/RigidBody").global_transform.origin,Vector3.UP)
 		rotate(Vector3(0,1,0),PI/2)
-	#	global_transform.basis = def_trans
+		#global_transform.basis.x = def_trans.x
 	else:
 	#	global_transform.basis = def_trans
 	#	$Camera.transform.origin = def_trans_pos
 	#	$"Camera".transform = def_camera
 		if !free_cam:
-			transform = def_self
+			if lerping:
+				quat_of_self_def = Quat(def_self.basis)
+				quat_of_self = Quat(transform.basis)
+				quat_of_self = quat_of_self.slerp(quat_of_self_def,t_lerp)
+				transform.basis = Basis(quat_of_self)
+				t_lerp += delta * 1.4
+				print(t_lerp)
+				if t_lerp >= 1:
+					lerping = false
+					t_lerp = 0
+			else:
+				transform = def_self
 		else:
 			global_transform.basis = def_trans
+
--- vehicle.gd
+++ vehicle.gd
@@ -16,6 +16,11 @@ var dodged = false
 var t_rotation = 0
 var anim
 var cam_mode = 1
+var alternator = false
+var air_forward = false
+var midair_move = false
+var air_steering
+#const air_steering_const
 var copy_global_transfrom = global_transform
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,11 +30,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if is_on_floor() && midair_move:
+		midair_move = false
+		$"player-model/ball_cam_trans".lerping = true
+		$"player-model/ball_cam_trans".t_lerp = 0
 	if is_on_floor() && !Input.is_action_pressed("jump"):
 		jump_bool = false
 		dodged = false
 	steering = Input.get_axis("right","left") * steering_const * delta
 	engine_force = Input.get_axis("back","forward") * engine_const * delta
+	air_steering = Input.get_axis("air_rotate_left","air_rotate_right") * delta * 60
 	if Input.get_axis("back","forward") < -0.5:
 		engine_force *= 2.5 
 	#print("data: ",jump_bool,dodged,Input.is_action_just_pressed("jump"),Input.is_action_pressed("forward"), " so final decision weather to dodge is: ",Input.is_action_just_pressed("jump") && jump_bool == true&& Input.is_action_pressed("forward") && !dodged)
@@ -40,13 +50,34 @@ func _process(delta):
 		dodged = true
 		apply_central_impulse(global_transform.basis.x * 300)
 		print("dodged!!@")
-		
+		air_forward = false
+		#apply_torque_impulse(-global_transform.basis.z * 130)
 	if Input.is_action_just_pressed("jump") && is_on_floor() && !jump_bool:
 		print("jumped")
 		jump_bool = true
-
+		air_forward = false
 		apply_central_impulse(Vector3.UP * 250)
-
+	
+	if Input.is_action_pressed("left") && !is_on_floor():
+		apply_torque_impulse(global_transform.basis.y * 0.7)
+		midair_move = true
+	if Input.is_action_pressed("right") && !is_on_floor():
+		apply_torque_impulse(-global_transform.basis.y * 0.7)
+		midair_move = true
+	if Input.is_action_pressed("back") && !is_on_floor():
+		apply_torque_impulse(global_transform.basis.z * 0.4)
+		midair_move = true
+	if Input.is_action_just_pressed("forward") && !is_on_floor():
+		air_forward = true
+		midair_move = true
+	if Input.is_action_pressed("forward") && !is_on_floor() && air_forward:
+		apply_torque_impulse(-global_transform.basis.z * 0.4)
+		midair_move = true
+	if air_steering != 0 && !is_on_floor():
+		apply_torque_impulse(global_transform.basis.x * air_steering * 0.4)
+		midair_move = true
+	if (Input.is_action_just_pressed("air_rotate_left") || Input.is_action_just_pressed("air_rotate_right") || Input.is_action_just_pressed("forward") || Input.is_action_just_pressed("back") || Input.is_action_just_pressed("right") || Input.is_action_just_pressed("left")) && midair_move:
+		$"player-model/ball_cam_trans".def_trans = $"player-model/ball_cam_trans".global_transform.basis
 	if Input.is_action_just_pressed("change_cam"):
 		cam_mode = (cam_mode + 1) % 3
 	if cam_mode == 0:
@@ -62,19 +93,18 @@ func _process(delta):
 		$Label.visible = true
 		$"player-model/ball_cam_trans".is_ball_cam = true
 		#$TextureRect.visible = true
-	elif cam_mode == 1:
+	elif cam_mode == 1 && midair_move == false:
 		#$"player-model/ball_cam_trans/Camera".transform = def_camera
 		$Label.visible = false
 		$TextureRect.visible = false
 		$"player-model/ball_cam_trans".free_cam = false
 		$"player-model/ball_cam_trans".is_ball_cam = false
-	elif cam_mode == 2:
+	else:
 		$Label.visible = false
 		$TextureRect.visible = false
 		$"player-model/ball_cam_trans".is_ball_cam = false
 		$"player-model/ball_cam_trans".free_cam = true
-	print(cam_mode)
-
+	alternator = !alternator
 func _on_Timer_timeout():
 	$TextureRect.visible = !$TextureRect.visible
 
